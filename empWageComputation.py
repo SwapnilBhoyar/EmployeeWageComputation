7c5e2d00adf705575f9028c0c5b34478538a682f
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-06-27 22:17:07 +0530

    [Swapnil] Add : calculate employee wage

--- empWageComputation.py
+++ empWageComputation.py
@@ -12,14 +12,21 @@ print("Wellcome to employee wage computation")
 def checkAttendance():
     """
     Description:
-        This function determine if employee is present or absent
+        This function determine if employee is present or absent and calculate wage
     """
     
     isPresent = 1
+    dayHour = 0
+    wagePerHour = 20
+
     attendance = random.randint(0,1)
     if attendance == isPresent:
+        dayHour = 8
         print("Employee is present")
     else:
         print("Employee is absent")
+
+    employeeWage = wagePerHour * dayHour
+    print("Employee wage is:", employeeWage)
 
 checkAttendance()
