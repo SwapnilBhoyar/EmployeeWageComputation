236170eca22f062972a8ff189f41d01a277e4b6e
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-06-28 02:15:00 +0530

    [Swapnil] Add : claculate wage for a month

--- empWageComputation.py
+++ empWageComputation.py
@@ -2,32 +2,46 @@
 @Author: Swapnil Bhoyar
 @Date: 2021-06-27 20:36:00 
 @Last Modified by: Swapnil Bhoyar
-@Last Modified time: 2021-06-28 01:26:00
+@Last Modified time: 2021-06-28 02:09:00
 @Title : Calculate wage of employee
 """
 
 import random
 print("Wellcome to employee wage computation")
 
-def calculateWage(attendanceStatus):
+def calculateHour(attendanceStatus):
     """
     Description:
-        This function calculate wage
+        this function determine the work hours
+    Parameter:
+        attendanceStatus is used to determine work hour of employee
+    Return:
+        the functution return 8 or 4 or 0 value as work hour
     """
-    
-    dayHour = 0
-    wagePerHour = 20
-
     if attendanceStatus == isFullTime:
         dayHour = 8
     elif attendanceStatus == isPartTime:
         dayHour = 4 
     else:
-        print("Employee is absent")
+        dayHour = 0
+    return dayHour
 
-    employeeWage = wagePerHour * dayHour
-    print("Employee wage is:", employeeWage)
-
+def calculateWage():
+    """
+    Description:
+        this function calculate employee wage
+    Return:
+        this function return total employee wage of a month
+    """
+    wagePerHour = 20
+    workingDays = 20
+    totalWage = 0
+    for day in range(workingDays):
+        attendance = random.randint(0,2)
+        attendanceStatus = switcher.get(attendance)
+        workingHours = calculateHour(attendanceStatus)
+        totalWage += workingHours * wagePerHour
+    return totalWage
 
 absent = 0
 isFullTime = 1
@@ -39,6 +53,4 @@ switcher = {
     2: isFullTime,
 }
 
-attendance = random.randint(0,2)
-attendanceStatus = switcher.get(attendance)
-calculateWage(attendanceStatus)
+print("Total employee wage for month is:",calculateWage())
