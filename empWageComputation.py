82d82d2641980d1d99663e2d0871d8e3fd2ba880
SwapnilBhoyar <swapnilbhoyar95@gmail.com>
2021-06-28 01:28:19 +0530

    [Swapnil] Add : used switch to calculate wage

--- empWageComputation.py
+++ empWageComputation.py
@@ -2,28 +2,25 @@
 @Author: Swapnil Bhoyar
 @Date: 2021-06-27 20:36:00 
 @Last Modified by: Swapnil Bhoyar
-@Last Modified time: 2021-06-26 22:49:00
+@Last Modified time: 2021-06-28 01:26:00
 @Title : Calculate wage of employee
 """
 
 import random
 print("Wellcome to employee wage computation")
 
-def checkAttendance():
+def calculateWage(attendanceStatus):
     """
     Description:
-        This function determine if employee is present or absent and calculate wage
+        This function calculate wage
     """
     
-    isFullTime = 1
-    isPartTime = 2
     dayHour = 0
     wagePerHour = 20
 
-    attendance = random.randint(0,2)
-    if attendance == isFullTime:
+    if attendanceStatus == isFullTime:
         dayHour = 8
-    elif attendance == isPartTime:
+    elif attendanceStatus == isPartTime:
         dayHour = 4 
     else:
         print("Employee is absent")
@@ -31,4 +28,17 @@ def checkAttendance():
     employeeWage = wagePerHour * dayHour
     print("Employee wage is:", employeeWage)
 
-checkAttendance()
+
+absent = 0
+isFullTime = 1
+isPartTime = 2
+
+switcher = {
+    0: absent,
+    1: isFullTime,
+    2: isFullTime,
+}
+
+attendance = random.randint(0,2)
+attendanceStatus = switcher.get(attendance)
+calculateWage(attendanceStatus)
